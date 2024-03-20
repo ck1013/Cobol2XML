4f170a42f724470600a5b797d3160a2dc4f2bbd8
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:09 +0800

    improve

--- Cobol2XML/src/logger/MyLogger.java
+++ Cobol2XML/src/logger/MyLogger.java
@@ -1,27 +1,28 @@
 package logger;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
-import java.util.logging.Handler;
-import java.util.logging.ConsoleHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class MyLogger {
-    static private FileHandler fileTxt;
-    static private SimpleFormatter formatterTxt;
 
-    static private FileHandler fileHTML;
-    static private Formatter formatterHTML;
+    private static FileHandler fileTxt;
+    private static SimpleFormatter formatterTxt;
 
-    static public void setup() throws IOException {
+    private static FileHandler fileHTML;
+    private static MyFormatter formatterHTML;
 
-        // get the global logger to configure it
+    public static void setup() throws IOException {
+
+        // Get the global logger to configure it
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-        // suppress the logging output to the console
+        // Suppress the logging output to the console
         Logger rootLogger = Logger.getLogger("");
         Handler[] handlers = rootLogger.getHandlers();
         if (handlers[0] instanceof ConsoleHandler) {
@@ -29,17 +30,23 @@ public class MyLogger {
         }
 
         logger.setLevel(Level.INFO);
-        fileTxt = new FileHandler("Logging.txt");
-        fileHTML = new FileHandler("Logging.html");
 
-        // create a TXT formatter
+        // Check if the log files already exist and append to them instead of overwriting
+        if (Files.exists(Paths.get("Logging.txt"))) {
+            fileTxt = new FileHandler("Logging.txt", true);
+        } else {
+            fileTxt = new FileHandler("Logging.txt");
+        }
+
+        if (Files.exists(Paths.get("Logging.html"))) {
+            fileHTML = new FileHandler("Logging.html", true);
+        } else {
+            fileHTML = new FileHandler("Logging.html");
+        }
+
+        // Create a TXT formatter
         formatterTxt = new SimpleFormatter();
         fileTxt.setFormatter(formatterTxt);
         logger.addHandler(fileTxt);
 
-        // create an HTML formatter
-        formatterHTML = new MyFormatter();
-        fileHTML.setFormatter(formatterHTML);
-        logger.addHandler(fileHTML);
-    }
-}
+       
