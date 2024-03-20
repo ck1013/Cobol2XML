cefb6696578dc20e480cae7d7211f3da833c4fab
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:24 +0800

    improve

--- Cobol2XML/src/parse/CollectionParser.java
+++ Cobol2XML/src/parse/CollectionParser.java
@@ -1,6 +1,4 @@
 /*
- * @(#)CollectionParser.java	 1.0.0
- *
  * Copyright (c) 1999 Steven J. Metsker
  *
  * This library is free software; you can redistribute it and/or
@@ -16,113 +14,136 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package parse;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Parser;
+
+/**
+ * An abstract class for parsers that are a collection of other parsers.
+ */
 public abstract class CollectionParser extends Parser {
-	/**
-	 * the parsers this parser is a collection of
-	 */
-	protected ArrayList<Parser> subparsers = new ArrayList<Parser>();
-/**
- * Supports subclass constructors with no arguments.
- */
-protected CollectionParser() {
-}
-/**
- * Supports subclass constructors with a name argument
- *
- * @param   string   the name of this parser
- */
-protected CollectionParser(String name) {
-	super(name);
-}
-/**
- * A convenient way to construct a CollectionParser with the
- * given parser.
- */
-protected CollectionParser(Parser p) {
-	subparsers.add(p);;
-}
-/**
- * A convenient way to construct a CollectionParser with the
- * given parsers.
- */
-protected CollectionParser(Parser p1, Parser p2) {
-	subparsers.add(p1);
-	subparsers.add(p2);
-}
-/**
- * A convenient way to construct a CollectionParser with the
- * given parsers.
- */
-protected CollectionParser(Parser p1, Parser p2, Parser p3) {
-	subparsers.add(p1);
-	subparsers.add(p2);
-	subparsers.add(p3);
-}
-/**
- * A convenient way to construct a CollectionParser with the
- * given parsers.
- */
-protected CollectionParser(
-	Parser p1, 
-	Parser p2, 
-	Parser p3, 
-	Parser p4) {
-	//
-	subparsers.add(p1);
-	subparsers.add(p2);
-	subparsers.add(p3);
-	subparsers.add(p4);
-}
-/**
- * Adds a parser to the collection.
- *
- * @param   Parser   the parser to add
- *
- * @return   this
- */
-public CollectionParser add (Parser e) {
-	subparsers.add(e);
-	return this;
-}
-/**
- * Return this parser's subparsers.
- *
- * @return   Vector   this parser's subparsers
- */
-public ArrayList<Parser> getSubparsers() {
-	return subparsers;
-}
-/**
- * Helps to textually describe this CollectionParser.
- *
- * @returns   the string to place between parsers in 
- *            the collection
- */
-protected abstract String toStringSeparator();
-/*
- * Returns a textual description of this parser.
- */
-protected String unvisitedString(ArrayList<Parser> visited) {
-	StringBuffer buf = new StringBuffer("<");
-	boolean needSeparator = false;
-	// Enumeration<Parser> e = subparsers.elements();
-	// Creating object of type Enumeration<Parser> 
-    Enumeration<Parser> e = Collections.enumeration(subparsers); 
-	while (e.hasMoreElements()) {
-		if (needSeparator) {
-			buf.append(toStringSeparator());
-		}
-		Parser next = e.nextElement();
-		buf.append(next.toString(visited));
-		needSeparator = true;
-	}
-	buf.append(">");
-	return buf.toString();
-}
-}
+
+    /**
+     * The parsers that this parser is a collection of.
+     */
+    protected ArrayList<Parser> subparsers = new ArrayList<Parser>();
+
+    /**
+     * Supports subclass constructors with no arguments.
+     */
+    protected CollectionParser() {
+    }
+
+    /**
+     * Supports subclass constructors with a name argument.
+     *
+     * @param name the name of this parser
+     */
+    protected CollectionParser(String name) {
+        super(name);
+    }
+
+    /**
+     * A convenient way to construct a CollectionParser with the given parser.
+     *
+     * @param p the parser to add
+     */
+    protected CollectionParser(Parser p) {
+        subparsers.add(p);
+    }
+
+    /**
+     * A convenient way to construct a CollectionParser with the given parsers.
+     *
+     * @param p1 the first parser
+     * @param p2 the second parser
+     */
+    protected CollectionParser(Parser p1, Parser p2) {
+        subparsers.add(p1);
+        subparsers.add(p2);
+    }
+
+    /**
+     * A convenient way to construct a CollectionParser with the given parsers.
+     *
+     * @param p1 the first parser
+     * @param p2 the second parser
+     * @param p3 the third parser
+     */
+    protected CollectionParser(Parser p1, Parser p2, Parser p3) {
+        subparsers.add(p1);
+        subparsers.add(p2);
+        subparsers.add(p3);
+    }
+
+    /**
+     * A convenient way to construct a CollectionParser with the given parsers.
+     *
+     * @param p1 the first parser
+     * @param p2 the second parser
+     * @param p3 the third parser
+     * @param p4 the fourth parser
+     */
+    protected CollectionParser(Parser p1, Parser p2, Parser p3, Parser p4) {
+        subparsers.add(p1);
+        subparsers.add(p2);
+        subparsers.add(p3);
+        subparsers.add(p4);
+    }
+
+    /**
+     * A convenient way to construct a CollectionParser with the given parsers.
+     *
+     * @param parsers the parsers to add
+     */
+    protected CollectionParser(Parser... parsers) {
+        for (Parser parser : parsers) {
+            subparsers.add(parser);
+        }
+    }
+
+    /**
+     * Adds a parser to the collection.
+     *
+     * @param e the parser to add
+     * @return this
+     */
+    public CollectionParser add(Parser e) {
+        if (e != null) {
+            subparsers.add(e);
+        }
+        return this;
+    }
+
+    /**
+     * Returns this parser's subparsers.
+     *
+     * @return the subparsers
+     */
+    public ArrayList<Parser> getSubparsers() {
+        return subparsers;
+    }
+
+    /**
+     * Helps to textually describe this CollectionParser.
+     *
+     * @return the string to place between parsers in the collection
+     */
+    protected abstract String getSeparator();
+
+    /**
+     * Returns a textual description of this parser.
+     *
+     * @param visited the parsers that have already been visited
+     * @return the string representation of this parser
+     */
+    protected String unvisitedString(ArrayList<Parser> visited) {
+        StringBuffer buf = new StringBuffer("<");
+        boolean needSeparator = false;
+        Iterator<Parser> iterator = subparsers.iterator();
+        while (iterator.hasNext())
