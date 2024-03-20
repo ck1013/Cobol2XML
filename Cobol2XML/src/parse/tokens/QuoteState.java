1199b8b18da9809078bab5d1fa763aeede873d51
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:15 +0800

    improve

--- Cobol2XML/src/parse/tokens/QuoteState.java
+++ Cobol2XML/src/parse/tokens/QuoteState.java
@@ -1,5 +1,5 @@
 /*
- * @(#)QuoteState.java	 1.0.0
+ * QuoteState.java
  *
  * Copyright (c) 1999 Steven J. Metsker
  *
@@ -16,49 +16,48 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package parse.tokens;
 
-import java.io.*;
-public class QuoteState extends TokenizerState {
-	protected char charbuf[] = new char[16];
-/*
- * Fatten up charbuf as necessary.
- */
-protected void checkBufLength(int i) {
-	if (i >= charbuf.length) {
-		char nb[] = new char[charbuf.length * 2];
-		System.arraycopy(charbuf, 0, nb, 0, charbuf.length);
-		charbuf = nb;
-	}
-}
+import java.io.IOException;
+import java.io.PushbackReader;
+
 /**
- * Return a quoted string token from a reader. This method 
- * will collect characters until it sees a match to the
- * character that the tokenizer used to switch to this 
- * state.
- *
- * @return a quoted string token from a reader
+ * A state that returns a quoted string token from a reader. This class collects
+ * characters until it sees a match to the character that the tokenizer used to
+ * switch to this state.
  */
-public Token nextToken(
-	PushbackReader r, int cin, Tokenizer t)
-	throws IOException {
-		
-	int i = 0;
-	charbuf[i++] = (char) cin;
-	int c;
-	do {
-		c = r.read();
-		if (c < 0) {
-			c = cin;
-		}
-		checkBufLength(i);
-		charbuf[i++] = (char) c;
-	} while (c != cin);
-	 
-	String sval = String.copyValueOf(charbuf, 0, i);
-	return new Token(Token.TT_QUOTED, sval, 0);
-}
-}
+public class QuoteState extends TokenizerState {
+
+    /** The character buffer used to store the characters of the quoted string. */
+    protected char[] charbuf = new char[16];
+
+    /**
+     * Fatten up charbuf as necessary.
+     *
+     * @param i the index to check against the buffer length
+     */
+    protected void checkBufLength(int i) {
+        if (i >= charbuf.length) {
+            char nb[] = new char[charbuf.length * 2];
+            System.arraycopy(charbuf, 0, nb, 0, charbuf.length);
+            charbuf = nb;
+        }
+    }
+
+    /**
+     * Returns a quoted string token from a reader.
+     *
+     * @param r the reader to read from
+     * @param cin the input character
+     * @param t the tokenizer object
+     * @return a quoted string token from a reader
+     * @throws IOException if an I/O error occurs
+     */
+    public Token nextToken(PushbackReader r, int cin, Tokenizer t) throws IOException {
+        int i = 0;
+        charbuf[i++] = (char) cin;
+        int c;
+        do {
+            c =
