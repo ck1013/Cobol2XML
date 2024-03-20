2494dc466f3780bd510a0ecc674d3ada300e1104
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:22 +0800

    improve

--- Cobol2XML/src/parse/tokens/Symbol.java
+++ Cobol2XML/src/parse/tokens/Symbol.java
@@ -1,7 +1,5 @@
 /*
- * @(#)Symbol.java	 1.0.0
- *
- * Copyright (c) 1999 Steven J. Metsker
+ * Symbol.java
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,71 +14,86 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package parse.tokens;
 
-import java.util.*;
-import parse.*;
+import java.util.Vector;
+import parse.Token;
+import parse.Parser;
 
+/**
+ * A Symbol is a parser that matches a specific token.
+ */
 public class Symbol extends Terminal {
-/**
- * the literal to match
- */
-	protected Token symbol; 
 
-/**
- * Constructs a symbol that will match the specified char.
- *
- * @param   char   the character to match. The char must be 
- *                 one that the tokenizer will return as a 
- *                 symbol token. This typically includes most 
- *                 characters except letters and digits. 
- *
- * @return   a symbol that will match the specified char
- */
-public Symbol(char c) {
-	this(String.valueOf(c));
-}
-/**
- * Constructs a symbol that will match the specified sequence
- * of characters.
- *
- * @param   String   the characters to match. The characters
- *                   must be a sequence that the tokenizer will 
- *                   return as a symbol token, such as
- *                   <code><=</code>.
- *
- * @return   a Symbol that will match the specified sequence
- *           of characters
- */
-public Symbol(String s) {
-	symbol = new Token(Token.TT_SYMBOL, s, 0);
-}
-/**
- * Returns true if the symbol this object represents equals an
- * assembly's next element.
- *
- * @param   object   an element from an assembly
- *
- * @return   true, if the specified symbol equals the next 
- *           token from an assembly
- */
-protected boolean qualifies(Object o) {
-	return symbol.equals((Token) o);
-}
-/**
- * Returns a textual description of this parser.
- *
- * @param   vector   a list of parsers already printed in 
- *                   this description
- * 
- * @return   string   a textual description of this parser
- *
- * @see Parser#toString()
- */
-public String unvisitedString(ArrayList<Parser> visited) {
-	return symbol.toString();
-}
+    // the literal to match
+    protected Token symbol;
+
+    /**
+     * Constructs a symbol that will match the specified character.
+     *
+     * @param c the character to match. The char must be one that the
+     * tokenizer will return as a symbol token. This typically includes
+     * most characters except letters and digits.
+     *
+     * @return a symbol that will match the specified character
+     */
+    public Symbol(char c) {
+        this(new Token(Token.TT_SYMBOL, String.valueOf(c), 0));
+    }
+
+    /**
+     * Constructs a symbol that will match the specified sequence of
+     * characters.
+     *
+     * @param s the characters to match. The characters must be a
+     * sequence that the tokenizer will return as a symbol token, such
+     * as "<=".
+     *
+     * @return a Symbol that will match the specified sequence of
+     * characters
+     */
+    public Symbol(String s) {
+        this(new Token(Token.TT_SYMBOL, s, 0));
+    }
+
+    /**
+     * Constructs a symbol that will match the specified token.
+     *
+     * @param symbol the token to match
+     */
+    public Symbol(Token symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * Returns true if the symbol this object represents equals an
+     * assembly's next element.
+     *
+     * @param o an element from an assembly
+     *
+     * @return true, if the specified symbol equals the next token from
+     * an assembly
+     */
+    protected boolean qualifies(Object o) {
+        if (o == null || !(o instanceof Token)) {
+            return false;
+        }
+        return symbol.equals(o);
+    }
+
+    /**
+     * Returns a textual description of this parser.
+     *
+     * @param visited a list of parsers already printed in this
+     * description
+     *
+     * @return string a textual description of this parser
+     *
+     * @see Parser#toString
+     */
+    public String unvisitedString(Vector<Parser> visited) {
+        return symbol.toString();
+    }
 }
