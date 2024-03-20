6d1c13ee5e279a342c9a877962509590b0336344
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:10:55 +0800

    improve

--- Cobol2XML/src/parse/tokens/SlashStarState.java
+++ Cobol2XML/src/parse/tokens/SlashStarState.java
@@ -1,5 +1,5 @@
 /*
- * @(#)SlashStarState.java	 1.0.0
+ * SlashStarState.java
  *
  * Copyright (c) 1999 Steven J. Metsker
  *
@@ -16,32 +16,35 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package parse.tokens;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PushbackReader;
+
 public class SlashStarState extends TokenizerState {
-/**
- * Ignore everything up to a closing star and slash, and 
- * then return the tokenizer's next token.
- *
- * @return the tokenizer's next token
- */
-public Token nextToken(
-	PushbackReader r, int theStar, Tokenizer t) 
-	throws IOException {
-		
-	int c = 0;
-	int lastc = 0;
-	while (c >= 0) {
-		if ((lastc == '*') && (c == '/')) {
-			break;
-		}
-		lastc = c;
-		c = r.read();
-	}
-	return t.nextToken();
-}
-}
+
+    /**
+     * Ignore everything up to a closing star and slash, and then return the
+     * tokenizer's next token.
+     *
+     * @param r
+     *            the PushbackReader to read from
+     * @param theStar
+     *            the position of the opening slash-star
+     * @param t
+     *            the Tokenizer to get the next token from
+     * @return the tokenizer's next token
+     * @throws IOException
+     *             if there is an error reading from the PushbackReader
+     */
+    public Token nextToken(PushbackReader r, int theStar, Tokenizer t)
+            throws IOException {
+        int c = 0;
+        int lastc = 0;
+        while (c >= 0) {
+            if ((lastc == '*') && (c == '/')) {
+                break;
+            }
+           
