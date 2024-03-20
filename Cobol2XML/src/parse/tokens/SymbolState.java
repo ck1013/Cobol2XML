05c08bfd6c1aa29e3cd71e9518e664fd0d5b3d9c
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:04 +0800

    improve

--- Cobol2XML/src/parse/tokens/SymbolState.java
+++ Cobol2XML/src/parse/tokens/SymbolState.java
@@ -1,7 +1,10 @@
-/*
- * @(#)SymbolState.java	 1.0.0
- *
- * Copyright (c) 1999 Steven J. Metsker
+package parse.tokens;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+
+/**
+ * A state for recognizing symbols in a tokenizer.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,45 +19,52 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
-package parse.tokens;
-
-import java.io.*;
 public class SymbolState extends TokenizerState {
-	SymbolRootNode symbols = new SymbolRootNode();
-/**
- * Constructs a symbol state with a default idea of what 
- * multi-character symbols to accept (as described in the 
- * class comment).
- *
- * @return   a state for recognizing a symbol
- */
-public SymbolState() {
-	add("!=");
-	add(":-");
-	add("<=");
-	add(">=");
-}
-/**
- * Add a multi-character symbol.
- *
- * @param   String   the symbol to add, such as "=:="
- */
-public void add(String s) {
-	symbols.add(s);
-}
-/**
- * Return a symbol token from a reader.
- *
- * @return a symbol token from a reader
- */
-public Token nextToken(
-	PushbackReader r, int first, Tokenizer t) 
-	throws IOException {
 
-	String s = symbols.nextSymbol(r, first); 
-	return new Token(Token.TT_SYMBOL, s, 0);
-}
-}
+    SymbolRootNode symbols = new SymbolRootNode();
+
+    /**
+     * Constructs a symbol state with a default idea of what
+     * multi-character symbols to accept (as described in the
+     * class comment).
+     *
+     * @return   a state for recognizing a symbol
+     */
+    public SymbolState() {
+        add("!=");
+        add(":-");
+        add("<=");
+        add(">=");
+    }
+
+    /**
+     * Add a multi-character symbol.
+     *
+     * @param   String   the symbol to add, such as "=:="
+     */
+    public void add(String s) {
+        symbols.add(s);
+    }
+
+    /**
+     * Remove a multi-character symbol.
+     *
+     * @param   String   the symbol to remove, such as "=:="
+     */
+    public void remove(String s) {
+        symbols.remove(s);
+    }
+
+    /**
+     * Return a symbol token from a reader.
+     *
+     * @return a symbol token from a reader
+     */
+    public Token nextToken(PushbackReader r, int first, Tokenizer t) throws IOException {
+        if (r == null) {
+            throw new IllegalArgumentException("PushbackReader r cannot be null");
+        }
+
+        String s = symbols.nextSymbol(r, first);
+        if (s != null && !s
