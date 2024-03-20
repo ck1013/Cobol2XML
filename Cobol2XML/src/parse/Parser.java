7ea4ec28f670f522f4e791f5913754a4a2db4323
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:44 +0800

    comment

--- Cobol2XML/src/parse/Parser.java
+++ Cobol2XML/src/parse/Parser.java
@@ -1,22 +1,14 @@
 /*
- * @(#)Parser.java	 1.0.0
- *
- * Copyright (c) 1999 Steven J. Metsker
- *
+ * Parser.java
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * License as published by the Free Software Foundation.
+ * 
+ * This parser is an abstract class that serves as a base for other
+ * parser implementations. It contains a name for identification and
+ * an Assembler object that will process an assembly when this
+ * parser successfully matches against it.
  */
 
 package parse;
@@ -28,62 +20,56 @@ import java.util.HashSet;
 
 public abstract class Parser {
     /*
-     * a name to identify this parser
+     * The name of this parser
      */
     private String name;
 
     /*
-     * an object that will work on an assembly whenever this
-     * parser successfully matches against the assembly
+     * An Assembler object that will process an assembly when this
+     * parser successfully matches against it
      */
     protected Assembler assembler;
 
     /**
-     * Constructs a nameless parser.
+     * Constructs a Parser object with an empty name.
      */
     protected Parser() {
         this.name = "";
     }
 
     /**
-     * Constructs a parser with the given name.
+     * Constructs a Parser object with the given name.
      *
-     * @param   String   A name to be known by. For parsers
-     *                   that are deep composites, a simple name
-     *                   identifying its purpose is useful.
+     * @param name A name to be known by.
      */
     protected Parser(String name) {
         this.name = name;
     }
 
     /**
-     * Accepts a "visitor" which will perform some operation on
-     * a parser structure. The book, "Design Patterns", explains
-     * the visitor pattern.
+     * Accepts a ParserVisitor object that will perform some
+     * operation on a parser structure.
      *
-     * @param   ParserVisitor   the visitor to accept
+     * @param pv The ParserVisitor object to accept
      */
     public void accept(ParserVisitor pv) {
         accept(pv, new ArrayList<Assembly>());
     }
 
     /**
-     * Accepts a "visitor" along with a collection of previously
-     * visited parsers.
+     * Accepts a ParserVisitor object along with a collection of
+     * previously visited parsers.
      *
-     * @param   ParserVisitor   the visitor to accept
-     *
-     * @param   Vector   a collection of previously visited
-     *                   parsers.
+     * @param pv The ParserVisitor object to accept
+     * @param visited A collection of previously visited parsers
      */
     public abstract void accept(ParserVisitor pv, ArrayList<Assembly> visited);
 
     /**
-     * Adds the elements of one vector to another.
+     * Adds the elements of one ArrayList to another.
      *
-     * @param   al1   the ArrayList to add to
-     *
-     * @param   al2   the ArrayList with elements to add
+     * @param al1 The ArrayList to add to
+     * @param al2 The ArrayList with elements to add
      */
     public static void add(ArrayList<Assembly> al1, ArrayList<Assembly> al2) {
         Enumeration<Assembly> e = Collections.enumeration(al2);
@@ -96,9 +82,8 @@ public abstract class Parser {
     /**
      * Returns the most-matched assembly in a collection.
      *
-     * @return   the most-matched assembly in a collection.
-     *
-     * @param   ArrayList   the collection to look through
+     * @param v The collection to look through
+     * @return The most-matched assembly in a collection
      */
     protected Assembly best(ArrayList<Assembly> v) {
         Assembly best = null;
@@ -122,10 +107,9 @@ public abstract class Parser {
      * Returns an assembly with the greatest possible number of
      * elements consumed by matches of this parser.
      *
-     * @return   an assembly with the greatest possible number of
-     *           elements consumed by this parser
-     *
-     * @param   Assembly   an assembly to match against
+     * @param a An assembly to match against
+     * @return An assembly with the greatest possible number of
+     * elements consumed by this parser
      */
     public Assembly bestMatch(Assembly a) {
         ArrayList<Assembly> in = new ArrayList<Assembly>();
@@ -138,14 +122,16 @@ public abstract class Parser {
      * Returns either null, or a completely matched version of
      * the supplied assembly.
      *
-     * @return   either null, or a completely matched version of the
-     *           supplied assembly
-     *
-     * @param   Assembly   an assembly to match against
+     * @param a An assembly to match against
+     * @return Either null, or a completely matched version of the
+     * supplied assembly
      */
     public Assembly completeMatch(Assembly a) {
         Assembly best = bestMatch(a);
         if (best != null && !best.hasMoreElements()) {
             return best;
         }
-       
+        // ...
+    }
+    // ...
+}
