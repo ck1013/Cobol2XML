f5c1d508ef8fea04ecfd15a052916527749ebce9
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:30 +0800

    improve

--- Cobol2XML/src/utensil/SwingUtensil.java
+++ Cobol2XML/src/utensil/SwingUtensil.java
@@ -1,7 +1,7 @@
 /*
- * @(#)SwingUtensil.java	 1.0.0
+ * SwingUtensil.java - Utility class for Swing components
  *
- * Copyright (c) 1999 Steven J. Metsker
+ * Copyright (c) 1999, 2023 Steven J. Metsker
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,9 +16,8 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package utensil;
 
 import java.awt.*;
@@ -27,119 +26,133 @@ import javax.swing.*;
 import javax.swing.border.*;
 
 public class SwingUtensil {
-/*
- * Set the location of a frame to be the center of the
- * screen.
- */
-public static void center(Frame f) {
-	Dimension sDim = 
-		Toolkit.getDefaultToolkit().getScreenSize();
-		
-	Dimension fDim = f.getSize();
-	if (fDim.height > sDim.height) {
-		fDim.height = sDim.height;
-	}
-	if (fDim.width > sDim.width) {
-		fDim.width = sDim.width;
-	}
-	
-	f.setLocation(
-		(sDim.width - fDim.width) / 2, 
-		(sDim.height - fDim.height) / 2);
-}
-/**
- * A standard font for interactive development environment
- * text areas.
- */
-public static Font ideFont() {
-	int style = Font.PLAIN;
-	int size = 18;
-	String name = "Verdana";
-	Font f = new Font(name, style, size);
-	if (!f.getFamily().equals(name)) {
-		name = "Monospaced";
-		f = new Font(name, style, size);
-	}
-	return f;
-}
-/**
- * Return a standard text area, with the standard font
- * and border, and with word wrapping enabled.
- */
-public static JTextArea ideTextArea() {
-	JTextArea ta = new JTextArea();
-	ta.setFont(ideFont());
-	ta.setBorder(new EmptyBorder(5, 5, 5, 5));
-	ta.setLineWrap(true);
-	return ta;
-}
-/**
- * Return a standard titled border.
- */
-public static TitledBorder ideTitledBorder(String title) {
-	TitledBorder tb = BorderFactory.createTitledBorder(
-		BorderFactory.createBevelBorder(BevelBorder.RAISED), 
-		title, 
-		TitledBorder.LEFT, 
-		TitledBorder.TOP);
-	tb.setTitleColor(Color.black);
-	tb.setTitleFont(ideFont());
-	return tb;
-}
-/**
- * Create a frame with the given title around the given
- * component; center and  display the frame; listen for
- * exit, closing the frame's window on exit.
- */
-public static JFrame launch(Component c, String title) {
-	JFrame frame = new JFrame(title);
-	frame.getContentPane().add(c);
-	SwingUtensil.listen(frame);
-	frame.pack();
-	SwingUtensil.center(frame);
-	frame.setVisible(true);
-	return frame;
-}
-/*
- * Set up an exit listener for a frame.
- */
-public static void listen(Frame f) {
-	f.addWindowListener(new WindowAdapter() {
-		@Override
-		public void windowClosing(WindowEvent e) {
-			System.exit(0);
-		};
-	});
-}
-/**
- * Returns a standard text panel, with a scroll pane around 
- * a text area, and with a title border.
- *
- * @param String the panel title
- *
- * @param JTextArea the text area to wrap
- *
- * @param Dimension the preferred size for this panel
- *
- * @param Dimension the minimum size for this panel
- *
- * @return a standard text panel, with a scroll pane 
- *         around a text area, and with a title border.
- */
-public static JPanel textPanel(
-	String title, JTextArea ta, 
-	Dimension pref, Dimension min) {
- 
-	// scroll pane around text area
-	JScrollPane s1 = new JScrollPane(ta);
-	s1.setPreferredSize(pref);
-	s1.setMinimumSize(min);
 
-	// titled panel that contains scrolling text area
-	JPanel p = new JPanel();
-	p.setLayout(new BorderLayout());
-	p.setBorder(SwingUtensil.ideTitledBorder(title));
-	p.add(s1, "Center");
-	return p;
-}
-}
+    /**
+     * Set the location of a frame to be the center of the screen.
+     *
+     * @param f the frame to center
+     */
+    public static void center(Frame f) {
+        if (f == null) {
+            throw new NullPointerException("Frame cannot be null");
+        }
+
+        Dimension sDim = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension fDim = f.getSize();
+
+        if (fDim.height > sDim.height) {
+            fDim.height = sDim.height;
+        }
+        if (fDim.width > sDim.width) {
+            fDim.width = sDim.width;
+        }
+
+        f.setLocation((sDim.width - fDim.width) / 2, (sDim.height - fDim.height) / 2);
+    }
+
+    /**
+     * A standard font for interactive development environment text areas.
+     *
+     * @return a standard font
+     */
+    public static Font ideFont() {
+        int style = Font.PLAIN;
+        int size = 18;
+        String name = "Verdana";
+        Font f = new Font(name, style, size);
+
+        if (!f.getFamily().equals(name)) {
+            switch (name) {
+                case "Verdana":
+                    name = "Monospaced";
+                    break;
+                case "Monospaced":
+                    name = "Courier New";
+                    break;
+                case "Courier New":
+                    name = "SansSerif";
+                    break;
+                default:
+                    name = "Serif";
+            }
+            f = new Font(name, style, size);
+        }
+        return f;
+    }
+
+    /**
+     * Return a standard text area, with the standard font and border, and with word wrapping enabled.
+     *
+     * @return a standard text area
+     */
+    public static JTextArea ideTextArea() {
+        JTextArea ta = new JTextArea();
+        ta.setFont(ideFont());
+        ta.setBorder(new EmptyBorder(5, 5, 5, 5));
+        ta.setLineWrap(true);
+        return ta;
+    }
+
+    /**
+     * Return a standard titled border.
+     *
+     * @param title the border title
+     * @return a standard titled border
+     */
+    public static TitledBorder ideTitledBorder(String title) {
+        TitledBorder tb = BorderFactory.createTitledBorder(
+                BorderFactory.createBevelBorder(BevelBorder.RAISED),
+                title,
+                TitledBorder.LEFT,
+                TitledBorder.TOP);
+        tb.setTitleColor(Color.black);
+        tb.setTitleFont(ideFont());
+        return tb;
+    }
+
+    /**
+     * Create a frame with the given title around the given component; center and display the frame; listen for exit, closing the frame's window on exit.
+     *
+     * @param c the component to add to the frame
+     * @param title the frame title
+     * @return the created frame
+     */
+    public static JFrame launch(Component c, String title) {
+        JFrame frame = new JFrame(title);
+        frame.getContentPane().add(c);
+        SwingUtensil.listen(frame);
+        frame.pack();
+        SwingUtensil.center(frame);
+        frame.setVisible(true);
+        return frame;
+    }
+
+    /**
+     * Set up an exit listener for a frame.
+     *
+     * @param f the frame to add the listener to
+     */
+    public static void listen(Frame f) {
+        if (f == null) {
+            throw new NullPointerException("Frame cannot be null");
+        }
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    /**
+     * Returns a standard text panel, with a scroll pane around a text area, and with a title border.
+     *
+     * @param title the panel title
+     * @param ta the text area to wrap
+     * @param pref the preferred size for this panel
+     * @param min the minimum size for this panel
+     * @return a standard text panel, with a scroll pane around a text area, and with a title border
+     */
+    public static JPanel textPanel(String title, JTextArea ta, Dimension pref, Dimension min) {
+        if (title == null || ta == null || pref ==
