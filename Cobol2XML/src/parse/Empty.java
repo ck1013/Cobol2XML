5ecf74ed35fb531e9f2e81776c8146db3a048445
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:43 +0800

    comment

--- Cobol2XML/src/parse/Empty.java
+++ Cobol2XML/src/parse/Empty.java
@@ -1,62 +1,71 @@
 /*
- * @(#)Empty.java	 1.0.0
+ * Empty.java - A parser that matches an empty input sequence.
  *
- * Copyright (c) 1999 Steven J. Metsker
+ * Copyright (c) 1999 Steven J. Metsker. This software is free
+ * software and is distributed under the terms of the GNU Lesser
+ * General Public License.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This parser is part of a library that is useful for matching
+ * and manipulating input sequences. It is distributed without any
+ * warranty.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * For more information, see the GNU Lesser General Public License.
  */
- 
+
 package parse;
 
 import java.util.*;
+
+/**
+ * An empty parser that matches an empty input sequence. This parser
+ * has no expansion and always succeeds in matching an empty input.
+ */
 public class Empty extends Parser {
-/**
- * Accept a "visitor" and a collection of previously visited
- * parsers.
- *
- * @param   ParserVisitor   the visitor to accept
- *
- * @param   ArrayList   a collection of previously visited parsers
- */
-public void accept(ParserVisitor pv, ArrayList<Assembly> visited) {
-	pv.visitEmpty(this, visited);
-}
-/**
- * Given a set of assemblies, this method returns the set as
- * a successful match.
- * 
- * @return   the input set of states
- *
- * @param   Vector   a vector of assemblies to match against
- *
- */
-public ArrayList<Assembly> match(ArrayList<Assembly> in) {
-	return elementClone(in);
-}
-/*
- * There really is no way to expand an empty parser, so
- * return an empty vector.
- */
-protected ArrayList<Assembly> randomExpansion(int maxDepth, int depth) {
-	return new ArrayList<Assembly>();
-}
-/*
- * Returns a textual description of this parser.
- */
- protected String unvisitedString(ArrayList<Parser> visited) {
-	return " empty ";
-}
+
+    /**
+     * Accepts a visitor and a collection of previously visited
+     * parsers.
+     *
+     * @param   ParserVisitor   the visitor to accept
+     * @param   ArrayList<Assembly>   a collection of previously visited parsers
+     */
+    public void accept(ParserVisitor pv, ArrayList<Assembly> visited) {
+        pv.visitEmpty(this, visited);
+    }
+
+    /**
+     * Given a set of assemblies, this method returns the set as
+     * a successful match.
+     *
+     * @return   the input set of states
+     *
+     * @param   Vector<Assembly>   a vector of assemblies to match against
+     */
+    public ArrayList<Assembly> match(ArrayList<Assembly> in) {
+        return elementClone(in);
+    }
+
+    /**
+     * Returns an empty vector since there is no way to expand an
+     * empty parser.
+     *
+     * @param   int   maxDepth, the maximum depth of the expansion
+     * @param   int   depth, the current depth of the expansion
+     *
+     * @return   an empty ArrayList<Assembly>
+     */
+    protected ArrayList<Assembly> randomExpansion(int maxDepth, int depth) {
+        return new ArrayList<Assembly>();
+    }
+
+    /**
+     * Returns a textual description of this parser.
+     *
+     * @param   ArrayList<Parser>   visited, a list of visited parsers
+     *
+     * @return   a string representation of this parser
+     */
+    protected String unvisitedString(ArrayList<Parser> visited) {
+        return " empty ";
+    }
 }
