f6f13184cac01980723e8db4fc0e55b15f818605
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:00 +0800

    improve

--- Cobol2XML/src/cobol/Program_idAssembler.java
+++ Cobol2XML/src/cobol/Program_idAssembler.java
@@ -1,5 +1,5 @@
 /*
- * @(#)Program_IDAssembler.java	 0.0.1
+ * Program_IDAssembler.java
  *
  * Copyright (c) 2019 Julian M. Bass
  *
@@ -16,24 +16,27 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
-package cobol;
 
-import parse.*;
-import parse.tokens.*;
-public class Program_idAssembler extends Assembler {
-/**
- * Pop a string, and set the target Program_ID to this
- * string.
- *
- * @param   Assembly   the assembly to work on
- */
-public void workOn(Assembly a) {
-	Cobol c = new Cobol();
-	Token t = (Token) a.pop();
-	c.setProgram_ID(t.sval().trim());
-	a.setTarget(c);
-}
-}
+package com.julianbass.cobol;
+
+import parse.Assembly;
+import parse.Token;
+import parse.tokens.StringToken;
+
+public class Program_IDAssembler extends Assembler {
+
+    /**
+     * Pop a string, and set the target Program_ID to this string.
+     *
+     * @param   assembly   the assembly to work on
+     */
+    @Override
+    public void workOn(Assembly assembly) {
+        Cobol cobol = new Cobol();
+        Token token = (Token) assembly.pop();
+        if (token instanceof StringToken) {
+            cobol.setProgram_ID(token.sval().trim());
+            assembly.setTarget(cobol);
+        } else {
+            throw new IllegalArgumentException("Expected a string token, but got: " + token
