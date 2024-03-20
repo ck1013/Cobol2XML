d52d45371ad7b1b28f7da1d8cdaffa1eea14bdcd
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:53 +0800

    comment

--- Cobol2XML/src/parse/tokens/SymbolState.java
+++ Cobol2XML/src/parse/tokens/SymbolState.java
@@ -1,37 +1,26 @@
-package parse.tokens;
-
-import java.io.IOException;
-import java.io.PushbackReader;
-
 /**
- * A state for recognizing symbols in a tokenizer.
+ * A state for recognizing symbols in a tokenizer. This class is responsible for 
+ * identifying and processing symbols in the input stream. It maintains a list of 
+ * multi-character symbols that it can recognize.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * This code is distributed under the GNU Lesser General Public License, which 
+ * allows for free use, redistribution, and modification, but comes with no 
+ * warranty.
  */
 public class SymbolState extends TokenizerState {
 
+    /**
+     * The root node of a trie data structure that stores the multi-character 
+     * symbols that this state can recognize.
+     */
     SymbolRootNode symbols = new SymbolRootNode();
 
     /**
-     * Constructs a symbol state with a default idea of what
-     * multi-character symbols to accept (as described in the
-     * class comment).
-     *
-     * @return   a state for recognizing a symbol
+     * Constructs a symbol state with a default set of multi-character symbols to 
+     * recognize. These symbols are: "!=", ":-", "<=", and ">=".
      */
     public SymbolState() {
+        // Adds the default set of multi-character symbols to the trie
         add("!=");
         add(":-");
         add("<=");
@@ -39,32 +28,43 @@ public class SymbolState extends TokenizerState {
     }
 
     /**
-     * Add a multi-character symbol.
+     * Adds a multi-character symbol to the trie data structure.
      *
-     * @param   String   the symbol to add, such as "=:="
+     * @param s the symbol to add, such as "=:="
      */
     public void add(String s) {
         symbols.add(s);
     }
 
     /**
-     * Remove a multi-character symbol.
+     * Removes a multi-character symbol from the trie data structure.
      *
-     * @param   String   the symbol to remove, such as "=:="
+     * @param s the symbol to remove, such as "=:="
      */
     public void remove(String s) {
         symbols.remove(s);
     }
 
     /**
-     * Return a symbol token from a reader.
+     * Reads the next token from the input stream, starting at the given character 
+     * position. If the next token is a multi-character symbol that this state can 
+     * recognize, it returns a new SymbolToken with the symbol's value. Otherwise, 
+     * it returns null.
      *
-     * @return a symbol token from a reader
+     * @return a symbol token from the input stream, or null if no symbol is 
+     *         recognized
      */
     public Token nextToken(PushbackReader r, int first, Tokenizer t) throws IOException {
         if (r == null) {
             throw new IllegalArgumentException("PushbackReader r cannot be null");
         }
 
+        // Reads the next symbol from the input stream using the trie data structure
         String s = symbols.nextSymbol(r, first);
-        if (s != null && !s
+        if (s != null && !s.isEmpty()) { // If a symbol was recognized
+            return new SymbolToken(s); // Returns a new SymbolToken with the symbol's value
+        } else {
+            return null; // Otherwise, returns null
+        }
+    }
+}
