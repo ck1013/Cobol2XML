a45c4c1dfa65a93d72ad798b5a2bd63c98044faa
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:35 +0800

    comment

--- Cobol2XML/cobol.cbl
+++ Cobol2XML/cobol.cbl
@@ -1,4 +1,5 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID.  BaseConverter.
+IDENTIFICATION DIVISION.  * This division defines the identification properties of the program such as the program name and date written.
+PROGRAM-ID.  BaseConverter.   * The name of the program is BaseConverter.
 
-DATE-WRITTEN.  07-MAR-199
+DATE-WRITTEN.  07-MAR-199   * The program was written on March 7, 1999.
+
