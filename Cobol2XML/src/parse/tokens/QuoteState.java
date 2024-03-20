c5980880e4848fc64dfc1d15095f5ac4554b88d3
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:39 +0800

    comment

--- Cobol2XML/src/parse/tokens/QuoteState.java
+++ Cobol2XML/src/parse/tokens/QuoteState.java
@@ -1,63 +1,31 @@
-/*
- * QuoteState.java
- *
- * Copyright (c) 1999 Steven J. Metsker
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-
-package parse.tokens;
-
-import java.io.IOException;
-import java.io.PushbackReader;
-
 /**
- * A state that returns a quoted string token from a reader. This class collects
- * characters until it sees a match to the character that the tokenizer used to
- * switch to this state.
+ * Returns a quoted string token from a reader.
+ * This method reads characters from the given reader and collects them until it finds a matching character
+ * for the quote that switched the tokenizer to this state.
+ *
+ * @param r          the reader to read from
+ * @param cin        the input character
+ * @param t          the tokenizer object
+ * @return           a quoted string token from a reader
+ * @throws IOException if an I/O error occurs
  */
-public class QuoteState extends TokenizerState {
+public Token nextToken(PushbackReader r, int cin, Tokenizer t) throws IOException {
+    int i = 0;
+    charbuf[i++] = (char) cin; // Store the initial quote character
+    int c;
+    do {
+        c = r.read(); // Read the next character from the reader
+        checkBufLength(i); // Ensure the character buffer has enough space
+        charbuf[i++] = (char) c; // Store the character in the buffer
+    } while (c != cin); // Continue until the matching quote character is found
 
-    /** The character buffer used to store the characters of the quoted string. */
-    protected char[] charbuf = new char[16];
+    // Create a new token with the collected characters (excluding the starting and ending quotes)
+    String lexeme = new String(charbuf, 1, i - 2);
+    Token token = new Token(TokenType.QUOTED_STRING, lexeme, t.getLine(), t.getColumn());
 
-    /**
-     * Fatten up charbuf as necessary.
-     *
-     * @param i the index to check against the buffer length
-     */
-    protected void checkBufLength(int i) {
-        if (i >= charbuf.length) {
-            char nb[] = new char[charbuf.length * 2];
-            System.arraycopy(charbuf, 0, nb, 0, charbuf.length);
-            charbuf = nb;
-        }
-    }
+    // Move the reader position past the ending quote
+    t.nextColumn(i - 1);
+    t.nextLine(r);
 
-    /**
-     * Returns a quoted string token from a reader.
-     *
-     * @param r the reader to read from
-     * @param cin the input character
-     * @param t the tokenizer object
-     * @return a quoted string token from a reader
-     * @throws IOException if an I/O error occurs
-     */
-    public Token nextToken(PushbackReader r, int cin, Tokenizer t) throws IOException {
-        int i = 0;
-        charbuf[i++] = (char) cin;
-        int c;
-        do {
-            c =
+    return token;
+}
