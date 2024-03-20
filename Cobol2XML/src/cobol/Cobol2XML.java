457171992af3bdaa994020bc8135f002dcf7b8b4
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:31 +0800

    comment

--- Cobol2XML/src/cobol/Cobol2XML.java
+++ Cobol2XML/src/cobol/Cobol2XML.java
@@ -9,55 +9,7 @@ import xmlwriter.*;
 
 public class Cobol2XML {
 
+    // Create a single Logger instance for the Cobol2XML class
     private static final Logger LOGGER = Logger.getLogger(Cobol2XML.class.getName());
 
-    public static void main(String[] args) {
-        try {
-            MyLogger.setup();
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Problems with creating the log files", e);
-            throw new RuntimeException(e);
-        }
 
-        LOGGER.setLevel(Level.INFO);
-        LOGGER.info("Cobol2XML V0.1.0");
-
-        if (args.length != 2) {
-            LOGGER.severe("Usage: java Cobol2XML <input_file> <output_file>");
-            return;
-        }
-
-        XMLPayload xmlp = new XMLPayload();
-
-        try (InputStream is = new FileInputStream(args[0]);
-             BufferedReader r = new BufferedReader(new InputStreamReader(is))) {
-
-            Tokenizer t = CobolParser.tokenizer();
-            Parser p = CobolParser.start();
-
-            String s;
-            while ((s = r.readLine()) != null) {
-                t.setString(s);
-                Assembly in = new TokenAssembly(t);
-                Assembly out = p.bestMatch(in);
-                Cobol c = new Cobol();
-                c = (Cobol) out.getTarget();
-
-                if (c != null) {
-                    xmlp.addElements(c);
-                }
-            }
-
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Error reading input file", e);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error processing cobol source", e);
-        }
-
-        try {
-            xmlp.writeFile(args[1]);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Error writing output file", e);
-        }
-    }
-}
