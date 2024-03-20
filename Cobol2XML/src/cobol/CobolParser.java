c4dcf631d1b7ec6854da2c9fef35f2d4d02d6dd5
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:28 +0800

    improve

--- Cobol2XML/src/cobol/CobolParser.java
+++ Cobol2XML/src/cobol/CobolParser.java
@@ -1,6 +1,4 @@
 /*
- * @(#)CobolParser.java	 0.1.0
- *
  * Copyright (c) 2019 Julian M. Bass
  *
  * This library is free software; you can redistribute it and/or
@@ -16,11 +14,12 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
+
 package cobol;
 
 import parse.Alternation;
+import parse.Assembler;
 import parse.Empty;
 import parse.Parser;
 import parse.Sequence;
@@ -31,122 +30,125 @@ import parse.tokens.Tokenizer;
 import parse.tokens.Word;
 
 public class CobolParser {
-	/**
-	 * Return a parser that will recognize the selected COBOL source code constructs:
-	 * 
-	 * 
-	 * This parser creates a <code>COBOL</code> object
-	 * as an assembly's target.
-	 *
-	 * @return a parser that will recognize and build a 
-	 *         <object>COBOL</object> from a source code file.
-	 */
-	public Parser cobol() {
-		Alternation a = new Alternation();
-		
-		Symbol fullstop = new Symbol('.');
-		fullstop.discard();
-		
-		a.add( ProgramID() );
-		
-		a.add( DivisionName() );
-		
-		a.add( SectionName() );
-		
-		a.add( DateWritten() );
-		
-		a.add(new Empty());
-		return a;
-	}
-	
-	/*
-	 * Return a parser that will recognize the grammar:
-	 * 
-	 *    Program Identifier = Word
-	 *
-	 */
-	protected Parser ProgramID() {
-		Sequence s = new Sequence();
-		s.add(new CaselessLiteral("program-id") );
-		s.add(new Symbol('.').discard());	
-		s.add(new Word().setAssembler(new Program_idAssembler()));
-		return s;
-	}
-
-
-
-	/*
-	 * Return a parser that will recognise the grammar:
-	 * 
-	 *    <divisionName> division
-	 *
-	 */
-	protected Parser DivisionName() {
-		Sequence s = new Sequence();
-		s.add(new Word().setAssembler(new DivisionAssembler()));
-		s.add(new CaselessLiteral("division") );
-		s.add(new Symbol('.').discard());
-		return s;
-	}
-	
-	/*
-	 * Return a parser that will recognize the grammar:
-	 * 
-	 *    Section Name = Word
-	 *
-	 */
-	protected Parser SectionName() {
-		Sequence s = new Sequence();
-		s.add(new Word().setAssembler(new SectionNameAssembler()));
-		s.add(new CaselessLiteral("section") );
-		s.add(new Symbol('.').discard());	
-
-		return s;
-	}
-	
-	/*
-	 * Return a parser that will recognise the grammar:
-	 * 
-	 *    working-storage section
-	 *
-	 */
-	protected Parser DateWritten() {
-		Sequence s = new Sequence();
-		s.add(new CaselessLiteral("date-written") );
-		s.add(new Symbol('.').discard());
-		s.add(new Num());
-		s.add(new Symbol('-').discard());
-
-		//This next Word actually contains month and year (which are extracted in DateAssembler
-		s.add(new Word());
-		s.add(new Symbol('-').discard());
-		s.add(new Word().discard());
-		s.add(new Symbol('.').discard());
-		s.setAssembler(new DateAssembler());
-		return s;
-	}
-
-
-	/**
-	 * Return the primary parser for this class -- cobol().
-	 *
-	 * @return the primary parser for this class -- cobol()
-	 */
-	public static Parser start() {
-		return new CobolParser().cobol();
-	}
-
-	/**
-	 * Returns a tokenizer that does not allow spaces to appear inside
-	 * the "words" that identify cobol's grammar.
-	 *
-	 * @return a tokenizer that does not allow spaces to appear inside
-	 * the "words" that identify cobol grammar.
-	 */
-	public static Tokenizer tokenizer() {
-		Tokenizer t = new Tokenizer();
-		t.wordState().setWordChars(' ', ' ', false);
-		return t;
-	}
-
-}
+    
+    public CobolParser() {
+        // constructor
+    }
+    
+    /**
+     * Return a parser that will recognize the selected COBOL source code constructs:
+     *
+     * This parser creates a COBOL object as an assembly's target.
+     *
+     * @return a parser that will recognize and build a COBOL object from a source code file.
+     */
+    public Parser cobol() {
+        Alternation a = new Alternation();
+        
+        Symbol fullstop = new Symbol('.');
+        fullstop.discard();
+        
+        a.add(ProgramID());
+        a.add(DivisionName());
+        a.add(SectionName());
+        a.add(DateWritten());
+        a.add(new Empty());
+        
+        return a;
+    }
+    
+    /**
+     * Return a parser that will recognize the grammar:
+     *
+     *    Program Identifier = Word
+     *
+     */
+    protected Parser ProgramID() {
+        Sequence s = new Sequence();
+        s.add(new CaselessLiteral("program-id"));
+        s.add(new Symbol('.').discard());
+        s.add(new Word().setAssembler(new Program_idAssembler()));
+        return s;
+    }
+    
+    /**
+     * Return a parser that will recognise the grammar:
+     *
+     *    <divisionName> division
+     *
+     */
+    protected Parser DivisionName() {
+        Sequence s = new Sequence();
+        s.add(new Word().setAssembler(new DivisionAssembler()));
+        s.add(new CaselessLiteral("division"));
+        s.add(new Symbol('.').discard());
+        return s;
+    }
+    
+    /**
+     * Return a parser that will recognize the grammar:
+     *
+     *    Section Name = Word
+     *
+     */
+    protected Parser SectionName() {
+        Sequence s = new Sequence();
+        s.add(new Word().setAssembler(new SectionNameAssembler()));
+        s.add(new CaselessLiteral("section"));
+        s.add(new Symbol('.').discard());
+        return s;
+    }
+    
+    /**
+     * Return a parser that will recognise the grammar:
+     *
+     *    working-storage section
+     *
+     */
+    protected Parser DateWritten() {
+        Sequence s = new Sequence();
+        s.add(new CaselessLiteral("date-written"));
+        s.add(new Symbol('.').discard());
+        s.add(new Num());
+        s.add(new Symbol('-').discard());
+        s.add(new Word());
+        s.add(new Symbol('-').discard());
+        s.add(new Word().discard());
+        s.add(new Symbol('.').discard());
+        s.setAssembler(new DateAssembler());
+        return s;
+    }
+    
+    /**
+     * Returns the primary parser for this class -- cobol().
+     *
+     * @return the primary parser for this class -- cobol
+     */
+    public static Parser start() {
+        return new CobolParser().cobol();
+    }
+    
+    /**
+     * Returns a tokenizer that does not allow spaces to appear inside
+     * the "words" that identify cobol's grammar.
+     *
+     * @return a tokenizer that does not allow spaces to appear inside
+     * the "words" that identify cobol grammar.
+     */
+    public static Tokenizer tokenizer() {
+        Tokenizer t = new Tokenizer();
+        t.wordState().setWordChars(' ', ' ', false);
+        return t;
+    }
+    
+    /**
+     * Parses the given COBOL source code using the COBOL parser.
+     *
+     * @param cobolSourceCode the COBOL source code to parse
+     * @return the parsed COBOL object
+     */
+    public COBOL parse(String cobolSourceCode) {
+        Tokenizer tokenizer = tokenizer();
+        tokenizer.input(cobolSourceCode);
+        Parser parser = cobol();
+        COBOL cobol = (COBOL) parser.parse(
