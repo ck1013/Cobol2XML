4c43f0f42ccf682b5a1e9915991a7762d5b0608b
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:11:06 +0800

    improve

--- Cobol2XML/src/parse/ParserVisitor.java
+++ Cobol2XML/src/parse/ParserVisitor.java
@@ -1,6 +1,6 @@
 /*
- * @(#)ParserVisitor.java	 1.0.0
- *
+ * ParserVisitor.java
+ * 
  * Copyright (c) 1999 Steven J. Metsker
  *
  * This library is free software; you can redistribute it and/or
@@ -16,58 +16,51 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package parse;
 
-import java.util.*;
+import java.util.ArrayList;
+
 public abstract class ParserVisitor {
-/**
- * Visit an alternation.
- *
- * @param   Alternation   the parser to visit
- *
- * @param   ArrayList   a collection of previously visited parsers
- *
- */
-public abstract void visitAlternation(
-	Alternation a, ArrayList<Assembly> visited);
-/**
- * Visit an empty parser.
- *
- * @param   Empty   the parser to visit
- *
- * @param   ArrayList   a collection of previously visited parsers
- *
- */
-public abstract void visitEmpty(Empty e, ArrayList<Assembly> visited);
-/**
- * Visit a repetition.
- *
- * @param   Repetition   the parser to visit
- *
- * @param   ArrayList   a collection of previously visited parsers
- *
- */
-public abstract void visitRepetition(
-	Repetition r, ArrayList<Assembly> visited);
-/**
- * Visit a sequence.
- *
- * @param   Sequence   the parser to visit
- *
- * @param   ArrayList   a collection of previously visited parsers
- *
- */
-public abstract void visitSequence(Sequence s, ArrayList<Assembly> visited);
-/**
- * Visit a terminal.
- *
- * @param   Terminal   the parser to visit
- *
- * @param   ArrayList   a collection of previously visited parsers
- *
- */
-public abstract void visitTerminal(Terminal t, ArrayList<Assembly> visited);
+
+    /**
+     * Visit an alternation.
+     *
+     * @param a     the parser to visit
+     * @param visited a collection of previously visited parsers
+     */
+    public abstract void visitAlternation(Alternation a, ArrayList<Assembly> visited);
+
+    /**
+     * Visit an empty parser.
+     *
+     * @param e     the parser to visit
+     * @param visited a collection of previously visited parsers
+     */
+    public abstract void visitEmpty(Empty e, ArrayList<Assembly> visited);
+
+    /**
+     * Visit a repetition.
+     *
+     * @param r     the parser to visit
+     * @param visited a collection of previously visited parsers
+     */
+    public abstract void visitRepetition(Repetition r, ArrayList<Assembly> visited);
+
+    /**
+     * Visit a sequence.
+     *
+     * @param s     the parser to visit
+     * @param visited a collection of previously visited parsers
+     */
+    public abstract void visitSequence(Sequence s, ArrayList<Assembly> visited);
+
+    /**
+     * Visit a terminal.
+     *
+     * @param t     the parser to visit
+     * @param visited a collection of previously visited parsers
+     */
+    public abstract void visitTerminal(Terminal t, ArrayList<Assembly> visited);
 }
