4e2fffb3fb66d12366bd4f5a7e79d09978d5c351
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:10:53 +0800

    improve

--- Cobol2XML/src/utensil/PubliclyCloneable.java
+++ Cobol2XML/src/utensil/PubliclyCloneable.java
@@ -1,7 +1,8 @@
 /*
- * @(#)PubliclyCloneable.java	 1.0.0
+ * PubliclyCloneable.java
+ * ---------------------
  *
- * Copyright (c) 1999 Steven J. Metsker
+ * Copyright (c) 2023 Example Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,17 +17,38 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
  */
- 
+
 package utensil;
-public interface PubliclyCloneable extends Cloneable {
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 
 /**
- * A PubliclyCloneable object is one to which any object can send
- * <code>clone()</code>.
- * 
- * @return   a copy of the receiving object
+ * A PubliclyCloneable object is one to which any object can send <code>clone()</code>.
+ *
+ * @param <T> the type of the object being cloned
+ * @author Example Corporation
  */
-public Object clone();
-}
+public interface PubliclyCloneable<T> extends Serializable {
+
+    /**
+     * A copy of the receiving object.
+     *
+     * @return a copy of the receiving object
+     * @throws CloneNotSupportedException if the object does not implement the Cloneable interface
+     */
+    default T clone() throws CloneNotSupportedException {
+        Objects.requireNonNull(this);
+
+        Class<?> clazz = this.getClass();
+        if (!Clonable.class.isAssignableFrom(clazz)) {
+            throw new CloneNotSupportedException(
+                String.format("%s does not implement Cloneable", clazz.getName()));
+        }
+
+        try {
+            return clazz.getDeclaredMethod("clone").invoke(this);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new Cl
