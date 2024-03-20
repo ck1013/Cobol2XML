567fbbd0ea3bfe1fe85c88e459d9f23d1ec2557a
ck1013 <147356983+ck1013@users.noreply.github.com>
2024-03-20 22:12:42 +0800

    comment

--- Cobol2XML/base.cbl
+++ Cobol2XML/base.cbl
@@ -1,12 +1,12 @@
-identification division.
+identification division.    * This section defines the identification properties of the program, such as the program name and author.
 
-program-id.  base_jb.
+program-id.  base_jb.       * The name of the program is base\_jb.
 
+date-written.  7-jul-1995 - mb.   * The program was written on July 7, 1995 by MB.
 
-date-written.  7-jul-1995 - mb.
+data division.               * This section defines the data that will be used in the program.
 
-data division.
-working-storage section.
+working-storage section.     * This subsection of the data division defines the working storage variables, which are variables that are only accessible within the current program.
 
-procedure division.
+procedure division.           * This section contains the main logic of the program.
 
